7c345f0e3e59ca2c95d94532b1f8d87e00648c0c
Mehedi Hasan <mehedihasan62086@gmail.com>
2025-09-30 23:24:18 +0600

    open browser in full screen mode for Playwright tests

--- conftest.py
+++ conftest.py
@@ -7,7 +7,11 @@ from utils import config
 @pytest.fixture(scope="function")
 def setup():
     with sync_playwright() as p:
-        browser = getattr(p, config.BROWSER).launch(headless=False)
-        page = browser.new_page()
+        browser = getattr(p, config.BROWSER).launch(
+            headless=False,
+            args=["--start-maximized"],
+        )
+        context = browser.new_context(no_viewport=True)
+        page = context.new_page()
         yield page
         browser.close()
